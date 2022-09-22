2fa110e1b257f3bfc49bb8a27074355c89a9b15d
Malado Mi'raj Diallo <Malado96@outlook.fr>
2022-09-22 10:25:35 +0100

    Footer Change, logo add

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -17,7 +17,7 @@
 
         <div class="col-lg-3 col-md-6 footer-links d-flex">
           <i class="bi bi-telephone icon"></i>
-           <div>
+          <div>
             <h4>Contact Sertem Groupe</h4>
             <p>
               <strong>Telephone:</strong> (+221) 33 869 77 67<br>
@@ -25,21 +25,22 @@
             </p>
           </div>
         </div>
+        <div class="col-lg-3 col-md-20 footer-links d-flex">
+          <a href="assets/img/footer/capture.png" class="glightbox"><img src="assets/img/footer/capture.png" class="menu-img img-fluid" alt=""></a>
+        </div>
+      </div>
 
+      <div class="container">
+        <div class="copyright">
+          &copy; Copyright <strong><span>Sertem Groupe</span></strong>. Tout droits reserves
+        </div>
+        <div class="credits">
+          <!-- All the links in the footer should remain intact. -->
+          <!-- You can delete the links only if you purchased the pro version. -->
+          <!-- Licensing information: https://bootstrapmade.com/license/ -->
+          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/yummy-bootstrap-restaurant-website-template/ -->
+          Designed by <a href=>Service IT Sertem Groupe</a>
+        </div>
       </div>
-    </div>
-
-    <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span>Sertem Groupe</span></strong>. Tout droits reserves
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/yummy-bootstrap-restaurant-website-template/ -->
-        Designed by <a href=>Service IT Sertem Groupe</a>
-      </div>
-    </div>
 
   </footer><!-- End Footer -->
